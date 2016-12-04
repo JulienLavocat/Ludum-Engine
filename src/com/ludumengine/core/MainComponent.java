8447ecb933c250f652f914aeee071b24e0868a04
JulienLavocat <julien.lavocat@gmail.com>
2016-12-04 22:49:03 +0100

    Window creating and updating

--- src/com/ludumengine/core/MainComponent.java
+++ src/com/ludumengine/core/MainComponent.java
@@ -8,6 +8,30 @@ public class MainComponent {
 	
 	public static void main(String args[]) {
 		Window.createWindow(WIDTH, HEIGHT, "Ludum Engine");
+		MainComponent game = new MainComponent();
+		game.start();
 	}
 
+	public void start() {
+		run();
+	}
+	
+	public void stop() {
+		
+	}
+	
+	public void run() {
+		while(!Window.isCloseRequested()) {
+			render();
+		}
+	}
+	
+	public void render() {
+		Window.render();
+	}
+	
+	public void cleanUp() {
+		
+	}
+	
 }
--- src/com/ludumengine/core/Window.java
+++ src/com/ludumengine/core/Window.java
@@ -10,13 +10,30 @@ public class Window {
 		try {
 			Display.setTitle(title);
 			Display.setDisplayMode(new DisplayMode(width, height));
+			Display.create();
 		} catch (LWJGLException e) {
 			e.printStackTrace();
 		}
 	}
 	
 	public static void render() {
-		
+		Display.update();
+	}
+	
+	public static boolean isCloseRequested() {
+		return Display.isCloseRequested();
+	}
+	
+	public static int getWidth() {
+		return Display.getDisplayMode().getWidth();
+	}
+	
+	public static int getHeight() {
+		return Display.getDisplayMode().getHeight();
+	}
+	
+	public static String getTitle() {
+		return Display.getTitle();
 	}
 
 }
